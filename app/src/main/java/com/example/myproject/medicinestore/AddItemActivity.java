e018541de8bbf1e734f77051f2a630b54ef6a5fd
nafsan <iamnafsan@gmail.com>
2019-08-07 15:21:32 +0600

    Multiple Objects of Database

--- app/src/main/java/com/example/myproject/medicinestore/AddItemActivity.java
+++ app/src/main/java/com/example/myproject/medicinestore/AddItemActivity.java
@@ -3,29 +3,39 @@ package com.example.myproject.medicinestore;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.firebase.client.Firebase;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import static android.content.ContentValues.TAG;
 
 public class AddItemActivity extends AppCompatActivity {
 
     private FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
     private DatabaseReference mDatabaseReference = mDatabase.getReference();
 
-    EditText name, description;
+    EditText name, description,price;
     Button submit;
     AutoCompleteTextView categoryTextView,diseaseTextView;
     private Firebase mRootRef;
-
+    private String pid= null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +53,8 @@ public class AddItemActivity extends AppCompatActivity {
         };
         String[] disease = {"Fever","Gastric","Ulcer","Cancer","Kidney","Heart Disease","Pain","Liver"};
 
+        FirebaseApp.initializeApp(this);
+
         categoryTextView = findViewById(R.id.category);
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.select_dialog_item, categories);
@@ -59,7 +71,7 @@ public class AddItemActivity extends AppCompatActivity {
 
         name = findViewById(R.id.medicine_id);
         description = findViewById(R.id.description);
-
+        price = findViewById(R.id.price);
         submit = findViewById(R.id.submit);
 
         submit.setOnClickListener(new View.OnClickListener() {
@@ -73,25 +85,7 @@ public class AddItemActivity extends AppCompatActivity {
                         .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
 
-                                String Mname = name.getText().toString();
-                                String Mdescription = description.getText().toString();
-                                String Mcategory = categoryTextView.getText().toString();
-                                String Mdisease = diseaseTextView.getText().toString();
-
-                                mRootRef = new Firebase("https://medicinestore-4131e.firebaseio.com/Medicine");
-
-                                mDatabaseReference = mDatabase.getReference().child("Medicine Name");
-                                mDatabaseReference.setValue(Mname);
-
-                                mDatabaseReference = mDatabase.getReference().child("Category");
-                                mDatabaseReference.setValue(Mcategory);
-
-                                mDatabaseReference = mDatabase.getReference().child("Description");
-                                mDatabaseReference.setValue(Mdescription);
-
-                                mDatabaseReference = mDatabase.getReference().child("Disease");
-                                mDatabaseReference.setValue(Mdisease);
-
+                                AddItem();
                                 Intent intent = new Intent(AddItemActivity.this, MainActivity.class);
                                 startActivity(intent);
 
@@ -107,5 +101,49 @@ public class AddItemActivity extends AppCompatActivity {
         });
 
     }
+    public void AddItem()
+    {
+        pid = FirebaseDatabase.getInstance().getReference().child("Medicine").push().getKey();
 
+        final String Mname = name.getText().toString();
+        final String Mdescription = description.getText().toString();
+        final String Mcategory = categoryTextView.getText().toString();
+        final String Mdisease = diseaseTextView.getText().toString();
+        final String Mprice = price.getText().toString();
+
+        mRootRef = new Firebase("https://medicinestore-4131e.firebaseio.com/Medicine");
+        FirebaseDatabase.getInstance().getReference().child("Medicine").push().getKey();
+
+        Query query = FirebaseDatabase.getInstance().getReference().child("Medicine").orderByChild("Medicine Name").equalTo(Mname);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+
+                if (!dataSnapshot.exists() && Mname!=null && Mcategory!=null && Mdisease!=null && Mdescription!=null && Mprice!=null) {
+                    mDatabaseReference.child(pid).child("Medicine Name").setValue(Mname);
+                    mDatabaseReference.child(pid).child("Category").setValue(Mcategory);
+                    mDatabaseReference.child(pid).child("Description").setValue(Mdescription);
+                    mDatabaseReference.child(pid).child("Disease").setValue(Mdisease);
+                    mDatabaseReference.child(pid).child("Price").setValue(Mprice);
+
+                    Toast.makeText(getApplicationContext(), "Item Added", Toast.LENGTH_SHORT).show();
+
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Item already added", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d(TAG, databaseError.getMessage());
+            }
+        });
+
+
+    }
 }
